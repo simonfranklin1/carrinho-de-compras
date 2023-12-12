5fdc26783b3324b10b41d3b12ed1ba5f903d014d
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2023-12-12 14:55:27 -0300

    Configurando deploy

--- src/utilidades/format.js
+++ src/utilidades/format.js
@@ -13,8 +13,4 @@ export const getLocalStorage = () => {
   const array = JSON.parse(localStorage.getItem('cart'));
 
   return array;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 50d1a6b1b96eb1cbb163d978b1eb050a798fd7a1
